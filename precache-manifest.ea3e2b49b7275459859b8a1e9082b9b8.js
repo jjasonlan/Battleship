50a5480a61a65995040c5151efbcfbf2c04b5522
Jason Lan <jason.lan@underarmour.com>
2019-11-17 17:34:28 -0800

    Updates

---
+++ precache-manifest.ea3e2b49b7275459859b8a1e9082b9b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d847948c66f4fb5e8b6b15ba4f3d80e",
+    "url": "/Battleship/index.html"
+  },
+  {
+    "revision": "1db0ad8a6dbe30ee0240",
+    "url": "/Battleship/static/css/main.d8ba0ffb.chunk.css"
+  },
+  {
+    "revision": "4a5b0b3c150633b9605a",
+    "url": "/Battleship/static/js/2.93bd3d70.chunk.js"
+  },
+  {
+    "revision": "1db0ad8a6dbe30ee0240",
+    "url": "/Battleship/static/js/main.73ee4740.chunk.js"
+  },
+  {
+    "revision": "47d674f2fdb76fd8d62e",
+    "url": "/Battleship/static/js/runtime-main.90e24e09.js"
+  }
+]);
